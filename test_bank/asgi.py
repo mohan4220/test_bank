f16216c1bbeadb772919bb6b1717934ffc9773fa
bandarup <mohan.bandarupalli_sai.ext@nokia.com>
2021-06-13 21:12:40 +0530

    next commit

--- test_bank/asgi.py
+++ test_bank/asgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 import os
-
+import sys
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'test_bank.settings')
